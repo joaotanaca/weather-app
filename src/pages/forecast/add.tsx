3080dbfac614709d2a5a69aa453c52d5df289b62
Tanaca <jtanacar@gmail.com>
2024-02-02 02:24:57 -0300

    delete: files unused

--- src/pages/forecast/add.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import AddLocation from "../../components/organisms/AddLocation";
-
-const AddForecast: React.FC = () => {
-    return <AddLocation />;
-};
-
-export default AddForecast;
