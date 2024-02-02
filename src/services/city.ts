8710b4bf2d2b79bed2930314118d0f17931487fa
Tanaca <jtanacar@gmail.com>
2024-02-02 02:31:00 -0300

    fix: type response in city service

--- src/services/city.ts
+++ src/services/city.ts
@@ -2,13 +2,13 @@ import { geolocationAPI } from "../http";
 import type { CityResponse } from "../interface/cityResponse";
 
 async function search(query: string) {
-    return await geolocationAPI.get<CityResponse>("/direct", {
+    return await geolocationAPI.get<CityResponse[]>("/direct", {
         params: { q: query },
     });
 }
 
 async function getCity(latitude: number, longitude: number) {
-    return await geolocationAPI.get<CityResponse>("/direct", {
+    return await geolocationAPI.get<CityResponse[]>("/reverse", {
         params: { lat: latitude, lon: longitude },
     });
 }
--- src/services/forecast.ts
+++ src/services/forecast.ts
@@ -12,18 +12,6 @@ async function today(latitude: string | number, longitude: string | number) {
     });
 }
 
-async function daily(latitude: string | number, longitude: string | number) {
-    return await forecastAPI.get<WeatherApiResponse>("forecast", {
-        params: {
-            latitude,
-            longitude,
-            daily: "weather_code,temperature_2m_max,temperature_2m_min,sunrise,sunshine_duration,precipitation_sum,rain_sum,showers_sum,precipitation_hours,precipitation_probability_max,wind_speed_10m_max,wind_gusts_10m_max,wind_direction_10m_dominant",
-            forecast_days: 14,
-        },
-    });
-}
-
 export default {
     today,
-    daily,
 };
