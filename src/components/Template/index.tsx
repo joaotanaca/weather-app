08b9f9f02500ffe36d23be595047c6fe2ac1466d
Tanaca <jtanacar@gmail.com>
2024-02-02 02:33:41 -0300

    feat: interfaces to city and forecast

--- src/components/Template/index.tsx
+++ src/components/Template/index.tsx
@@ -1,12 +1,14 @@
 import React, { useEffect } from "react";
 import { Outlet, useLocation, useNavigate } from "react-router-dom";
-import Navbar from "../organisms/Navbar";
+import Navbar from "./Navbar";
+import theme from "../../store/theme";
 
 const Template: React.FC = () => {
     const { pathname } = useLocation();
     const navigate = useNavigate();
 
     useEffect(() => {
+        theme.validateTheme();
         if (pathname === "/") navigate("forecasts");
     }, []);
 
--- src/interface/cityResponse.ts
+++ src/interface/cityResponse.ts
@@ -1,6 +1,8 @@
 export type CityResponse = {
+    id?: string;
     name: string;
     lat: number;
     lon: number;
     country: string;
+    state?: string;
 };
