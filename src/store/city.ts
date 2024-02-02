22f457052987817ed0c82aff0fd90ef4025828d6
Tanaca <jtanacar@gmail.com>
2024-02-02 02:31:13 -0300

    feat: add functionalities to city store

--- src/store/city.ts
+++ src/store/city.ts
@@ -1,28 +1,69 @@
-import { action, makeObservable, observable } from "mobx";
+import { action, makeObservable, observable, runInAction } from "mobx";
+import { AxiosError } from "axios";
 import { CityResponse } from "../interface/cityResponse";
 import city from "../services/city";
+import filterCities from "../helpers/filterCities";
+export type INITIAL_CITY_TYPE = typeof INITIAL_CITY;
+
+const INITIAL_CITY = {
+    id: null,
+    name: "",
+    lat: 0,
+    lon: 0,
+    country: "",
+    state: "",
+};
 
 class City {
     cities: CityResponse[] = [];
-    private timer: number | null = null;
+    citySelected: INITIAL_CITY_TYPE = INITIAL_CITY;
 
     constructor() {
         makeObservable(this, {
             cities: observable,
+            citySelected: observable,
             search: action.bound,
+            handleUpdateCitySelected: action.bound,
         });
     }
 
-    search = (query: string) => {
-        if (!query) {
-            this.cities = [];
-            return;
+    search = async (query: string) => {
+        let response: CityResponse[];
+        try {
+            if (this.citySelected.name) {
+                response = (await city.search(query)).data;
+            } else {
+                response = (
+                    await city.getCity(
+                        this.citySelected.lat,
+                        this.citySelected.lon,
+                    )
+                ).data;
+            }
+            runInAction(() => {
+                this.cities = filterCities(response);
+            });
+        } catch (err) {
+            console.error((err as AxiosError).message);
         }
-        if (this.timer) clearTimeout(this.timer);
-        this.timer = setTimeout(async () => {
-            const { data } = await city.search(query);
-            console.log(data);
-        }, 1000);
+    };
+
+    handleUpdateCitySelected = (
+        key: "city" | "name" | "lat" | "lon" | "country" | "state" | "all",
+        value: any,
+    ) => {
+        if (key === "all") {
+            this.citySelected = value as INITIAL_CITY_TYPE;
+        } else {
+            this.citySelected = { ...this.citySelected, [key]: value };
+        }
+    };
+
+    resetCitySelected = () => {
+        runInAction(() => {
+            this.citySelected = INITIAL_CITY;
+            this.cities = [];
+        });
     };
 }
 
