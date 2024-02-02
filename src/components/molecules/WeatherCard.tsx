25cc91efe83702d2781b60a612fb1807e346a7ff
Tanaca <jtanacar@gmail.com>
2024-02-02 02:34:17 -0300

    refactor: weather card

--- src/components/molecules/WeatherCard.tsx
+++ src/components/molecules/WeatherCard.tsx
@@ -1,60 +1,51 @@
 import React, { useCallback, useMemo, useState } from "react";
 import { TbReload } from "react-icons/tb";
 import Icon from "../atom/icon";
-import { observer } from "mobx-react";
-import forecasts from "../../store/forecasts";
-import convertCelsiusToFahreneit from "../../helpers/convertCelsiusToFahreneit";
+
 type TCard = {
     name: string;
+    className?: string;
+    temperature: string;
     weatherCode: string;
-    temperature: number;
-    handleClick: () => void;
+    handleRefresh: () => Promise<void>;
 };
 
-const Card = ({ name, temperature: temp, weatherCode, handleClick }: TCard) => {
+const WeatherCard = ({
+    name,
+    temperature,
+    className,
+    weatherCode,
+    handleRefresh,
+}: TCard) => {
     const [refresh, setRefresh] = useState(false);
 
-    const temperature = useMemo(
-        () =>
-            forecasts.scaleTemperature === "celsius"
-                ? `${temp}°C`
-                : `${convertCelsiusToFahreneit(temp)}°F`,
-        [forecasts.scaleTemperature],
-    );
-
-    const handleRefresh = useCallback(() => {
-        handleClick();
+    const handleClick = useCallback(async () => {
         setRefresh(true);
-        setTimeout(() => {
-            setRefresh(false);
-        }, 500);
+        await handleRefresh();
+        setRefresh(false);
     }, []);
 
     return (
         <article
-            className="w-60 flex flex-col items-center gap-5 p-6 
-        rounded-[20px] backdrop-blur-[34px] 
-        shadow-md hover:shadow-md hover:border-[#296399] hover:dark:border-white border-[#C9E5FF] dark:border-[#1B1B1D] border-[1px] 
-        dark:bg-[#1B1B1D] bg-[#C9E5FF] text-[#296399] dark:text-white 
-        font-nunito-sans transition-all cursor-pointer"
+            className={`${className} w-60 flex flex-col items-center gap-5 p-5 px-6 rounded-xl dark:bg-[#1B1B1D] bg-neutral-100 text-neutral-700 dark:text-white transition-all shadow-sm hover:shadow-md hover:-translate-y-1 hover:text-primary-600 border-1 hover:border-primary-600 border-neutral-700 md:border-none md:border-0`}
         >
             <header className="w-full flex justify-between text-lg font-semibold">
                 {name}
                 <button
-                    className={
+                    className={`${
                         refresh ? "animate-[spin_500ms_ease_infinite]" : ""
-                    }
-                    onClick={handleRefresh}
+                    }`}
+                    onClick={handleClick}
                 >
                     <TbReload size={20} />
                 </button>
             </header>
             <Icon name="fog" size={120} />
-            <footer className="flex justify-center text-2xl">
+            <footer className="flex justify-center text-2xl font-normal">
                 {temperature}
             </footer>
         </article>
     );
 };
 
-export default observer(Card);
+export default WeatherCard;
