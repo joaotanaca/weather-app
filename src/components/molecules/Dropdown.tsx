7c78ce8aaab49a0a08e65f63dfb099137c46f4b6
Tanaca <jtanacar@gmail.com>
2024-02-04 01:10:11 -0300

    feat: create dropdown component

---
+++ src/components/molecules/Dropdown.tsx
@@ -0,0 +1,23 @@
+import { FiMoreVertical } from "react-icons/fi";
+
+import React, { useRef, useState } from 'react';
+import Button from "../atom/Button";
+import Menu, { MenuRef, OptionsMenu } from "./Menu";
+
+type DropdownProps = {
+    className?: string
+    options: OptionsMenu
+}
+
+const Dropdown = ({ className, options }: DropdownProps) => {
+    let menuRef: MenuRef | null
+
+    return <div className="relative">
+        <Button className={className} onClick={() => menuRef?.open()} color="transparent">
+            <FiMoreVertical />
+        </Button>
+        <Menu ref={node => { menuRef = node as MenuRef }} options={options} />
+    </div >;
+}
+
+export default Dropdown;
