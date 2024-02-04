4e55a6ca41204a6690e72c92a864b5d637ae44cd
Tanaca <jtanacar@gmail.com>
2024-02-04 01:06:25 -0300

    feat: menu and item menu component

---
+++ src/components/atom/ItemMenu.tsx
@@ -0,0 +1,27 @@
+import React, { useMemo } from 'react';
+import Icon, { IconsName } from './icon';
+
+export type ItemMenu = {
+    icon?: IconsName | JSX.Element
+    className?: string
+    title: string
+    onClick?: () => void
+}
+const ItemMenu = ({ title, icon, onClick, className }: ItemMenu) => {
+    const IconMenu = useMemo(() => typeof icon === "string" ? <Icon name={icon} size={16} /> : icon, [])
+    const handleOnClick = () => {
+        onClick?.()
+        document.querySelectorAll('.menu').forEach((menu) => {
+            menu.classList.remove('active')
+        })
+    }
+    return <button onClick={handleOnClick} className={`${className} ${!className?.includes('hover:bg') && "hover:bg-neutral-300"} p-2 w-full text-neutral-900 dark:text-white flex items-center gap-1 rounded-[4px] text-base transition-all`} type='button'>
+        <div className='min-w-4 min-h-4'>
+            {IconMenu}
+        </div>
+        {title}
+    </button>;
+}
+
+export default ItemMenu;
+
---
+++ src/components/molecules/Menu.tsx
@@ -0,0 +1,26 @@
+import React, { forwardRef, useImperativeHandle, useMemo, useRef } from 'react';
+import type { ItemMenu } from '../atom/ItemMenu';
+import ItemMenuComponent from '../atom/ItemMenu';
+import { v4 as uuid } from 'uuid';
+
+export type OptionsMenu = ItemMenu[];
+export type MenuRef = { open: () => void };
+
+type MenuProps = {
+    className?: string
+    options: OptionsMenu
+}
+
+const Menu = forwardRef(({ options }: MenuProps, ref) => {
+    const menu = useMemo(() => options.map(option => <ItemMenuComponent key={uuid()} {...option} />), [options])
+    const menuRef = useRef<HTMLDivElement>(null)
+    useImperativeHandle(ref, () => ({
+        open() {
+            menuRef.current?.classList.toggle('active')
+        }
+    }), [])
+
+    return <div ref={menuRef} className='menu shadow-menu bg-white dark:bg-neutral-800 rounded-xl p-2 absolute top-full right-0'>{menu}</div>;
+})
+
+export default Menu;
